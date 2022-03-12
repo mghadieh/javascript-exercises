4227bde9b804573b8d0ee2f5ec031d10590599d8
mghadieh <mohamadghadieh@gmail.com>
2022-03-12 00:07:56 -0500

    03_reverseString Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+	let lengthOfString = str.length;
+	let result = '';
+	for (i = lengthOfString - 1; i >= 0; i--) {
+		result += str[i];
+	}
+	return result;
 };
 
 // Do not edit below this line
