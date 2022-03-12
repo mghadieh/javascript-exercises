39224b338d7cb2f4027a7f7c228697564c487307
mghadieh <mohamadghadieh@gmail.com>
2022-03-12 00:57:19 -0500

    05_sumAll Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (low, high) {
+	if (low < 0 || high < 0) return 'ERROR';
+	if (typeof low != 'number' || typeof high != 'number') return 'ERROR';
+	if (low > high) {
+		let temp = low;
+		low = high;
+		high = temp;
+	}
+	let result = 0;
+	for (let i = low; i <= high; i++) {
+		result += i;
+	}
+	return result;
 };
 
 // Do not edit below this line
