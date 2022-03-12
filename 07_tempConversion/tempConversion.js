a576d7c19d6dcea767a936eaa4de2a1c686cdd14
mghadieh <mohamadghadieh@gmail.com>
2022-03-12 14:14:13 -0500

    07_tempConversion Solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,22 @@
-const ftoc = function() {
+const ftoc = function (temp) {
+	let result = (temp - 32) * (5 / 9);
+	if (result - Math.floor(result) != 0)
+		result = Number.parseFloat(result.toFixed(1));
 
+	return result;
 };
 
-const ctof = function() {
+const ctof = function (temp) {
+	let result = (temp * 9) / 5 + 32;
 
+	if (result - Math.floor(result) != 0)
+		result = Number.parseFloat(result.toFixed(1));
+
+	return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
