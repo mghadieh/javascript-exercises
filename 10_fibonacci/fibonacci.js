fc2ec0ea619177d0df5825aa3faa297beaf53e06
mghadieh <mohamadghadieh@gmail.com>
2022-03-13 00:26:06 -0500

    10_fibonacci Solved! O(n) complexity"

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	let result = 1;
+	let pre = 1;
+	let current = 1;
+	num = parseInt(num);
+	if (num <= 0) return 'OOPS';
+	for (let i = 2; i < num; i++) {
+		result = pre + current;
+		pre = current;
+		current = result;
+	}
+	return result;
 };
 
 // Do not edit below this line
