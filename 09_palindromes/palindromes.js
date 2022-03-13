1332aa3c16c1be2f985f94943963c5e649a1638a
mghadieh <mohamadghadieh@gmail.com>
2022-03-12 23:50:53 -0500

    09_palindromes Solved!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = str
+		.toLowerCase()
+		.replace(/[^\w\s]|_/g, '')
+		.replace(/\s+/g, '');
+	let i = 0;
+	let j = str.length - 1;
+	while (true) {
+		if (i >= j) break;
+		if (str[i] === str[j]) {
+			i++;
+			j--;
+			continue;
+		}
+		return false;
+	}
+	return true;
 };
 
 // Do not edit below this line
